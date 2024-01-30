4aa21d23246ab6cb84da2065ef16861fe91f8514
이정한 <jason015634@gmail.com>
2024-01-30 11:29:20 +0900

    jsonSerilizer 커밋

--- 4Q_GameEngine/GameEditor/jsonSerializer.h
+++ 4Q_GameEngine/GameEditor/jsonSerializer.h
@@ -2,27 +2,27 @@
 #include <string>
 #include "nlohmann/json.hpp"
 
-// nlohmann::json 객체를 만드는 표현식인 nlohmann::json(container)이 nlohmann::json으로 변환이 가능해야함
-template<typename T>
-concept SerializeableContainer = requires(const T & container) {
-	{nlohmann::json(container)} -> std::convertible_to<nlohmann::json>;
-};
-
-
-template<typename Container>
-std::string SerializeableContainer(const Container& myContainer) requires SerializeableContainer<Container>
-{
-	nlohmann::json jsonObject = myContianer;
-	return jsonObject.dump(2);
-}
-
-// vector 리턴 
-template <typename Container>
-Container DeserializeContainerFromFile(const std::string& filename)
-{
-	std::ifstream inputFile(filename);
-	nlohmann::json jsonObject;
-	inputFile >> jsonObject;
-	inputFile.close();
-	return jsonObject.get<Container>();
-}
+//// nlohmann::json 객체를 만드는 표현식인 nlohmann::json(container)이 nlohmann::json으로 변환이 가능해야함
+//template<typename T>
+//concept SerializeableContainer = requires(const T & container) {
+//	{nlohmann::json(container)} -> std::convertible_to<nlohmann::json>;
+//};
+//
+//
+//template<typename Container>
+//std::string SerializeableContainer(const Container& myContainer) requires SerializeableContainer<Container>
+//{
+//	nlohmann::json jsonObject = myContianer;
+//	return jsonObject.dump(2);
+//}
+//
+//// vector 리턴 
+//template <typename Container>
+//Container DeserializeContainerFromFile(const std::string& filename)
+//{
+//	std::ifstream inputFile(filename);
+//	nlohmann::json jsonObject;
+//	inputFile >> jsonObject;
+//	inputFile.close();
+//	return jsonObject.get<Container>();
+//}
