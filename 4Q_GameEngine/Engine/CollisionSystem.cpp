8c6271a98397919a2bc26450e3c8e48fa7003292
heosumin518 <heosumin518@gmail.com>
2024-02-19 00:20:00 +0900

    Collider 자식 이동 에러 수정

--- 4Q_GameEngine/Engine/CollisionSystem.cpp
+++ 4Q_GameEngine/Engine/CollisionSystem.cpp
@@ -41,7 +41,7 @@ void CollisionSystem::Tick(World* world, ECS::DefaultTickData data)
 {
 	world->each<Transform, BoxCollider>([&](Entity* ent, ComponentHandle<Transform> transform, ComponentHandle<BoxCollider> collider)
 		{
-			transform->m_Position = collider->m_Center;
+			//transform->m_Position = collider->m_Center;
 			collider->m_IsRaycastHit = false;
 
 			//if (ent->get<EntityIdentifier>()->m_HasParent)
@@ -53,7 +53,19 @@ void CollisionSystem::Tick(World* world, ECS::DefaultTickData data)
 			//{
 			//	collider->m_Center = transform->m_Position;	// TODO: 24.02.16 임시 수정
 			//}
-			collider->m_Center = transform->m_WorldMatrix.ConvertToMatrix().Translation();
+
+			//collider->m_Center = transform->m_Position;
+
+			if (ent->get<EntityIdentifier>()->m_HasParent)
+			{
+				if(ent->m_parent->has<Transform>())
+					collider->m_Center = ent->getParent()->get<Transform>()->m_Position + transform->m_Position;
+			}
+			else
+			{
+				//collider->m_Center = transform->m_Position;
+				collider->m_Center = transform->m_WorldMatrix.ConvertToMatrix().Translation();
+			}
 
 			/*for (auto& child : ent->m_children)
 			{
