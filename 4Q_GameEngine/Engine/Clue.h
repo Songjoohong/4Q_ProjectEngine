4c2827f12bd273db1066f5efcad1c6c81fa8d6d1
Songjoohong <rksek16@naver.com>
2024-02-22 10:24:54 +0900

    2024.02.22

---
+++ 4Q_GameEngine/Engine/Clue.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "ECS.h"
+
+struct Clue
+{
+	ECS_DECLARE_TYPE
+		Clue() = default;
+	Clue(int page, int index, std::string path)
+		: m_Page(page)
+		, m_Index(index)
+		, m_FilePath(path)
+	{}
+	~Clue() = default;
+
+	int m_Page;
+	int m_Index;
+	std::string m_FilePath;
+};
+ECS_DEFINE_TYPE(Clue)
