28461957e3f4cd16e035d7a7e6af055914ae1647
Songjoohong <rksek16@naver.com>
2024-02-22 10:47:21 +0900

    2024.02.22

--- 4Q_GameEngine/Engine/ClueSystem.h
+++ 4Q_GameEngine/Engine/ClueSystem.h
@@ -1,5 +1,21 @@
 #pragma once
-class ClueSystem
-{
-};
+#include "ECS.h"
 
+using namespace ECS;
+
+class ClueSystem : public EntitySystem,
+	public EventSubscriber<Events::CluePageChange>,
+	public EventSubscriber<Events::ClueFound>
+{
+	virtual void Configure(World* world) override;
+	virtual void Deconfigure(World* world) override;
+
+	virtual void Receive(World* world, const Events::CluePageChange& event) override;
+	virtual void Receive(World* world, const Events::ClueFound& event) override;
+
+	virtual void Tick(World* world, ECS::DefaultTickData data) override;
+
+private:
+	bool m_IsClueOn = false;
+	int m_CurrentPage = 1;
+};
